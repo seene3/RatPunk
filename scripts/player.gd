6315283bc6d7d78a7225c5e5c58755a159bdca5e
Sean Gillespie <seene3@gmail.com>
2024-11-29 19:29:09 -0500

    first commit

---
+++ scripts/player.gd
@@ -0,0 +1,52 @@
+extends CharacterBody3D
+
+
+var current_speed = 5.0
+var lerp_speed = 10
+var direction = Vector3.ZERO
+const JUMP_VELOCITY = 4.5
+const walk_speed = 5.0
+const sprint_speed = 10.0
+
+@onready var pivot: Node3D = $CamOrigin
+@export var sens = 0.5
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * sens))
+		pivot.rotate_x(deg_to_rad(-event.relative.y * sens))
+		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(45))
+
+func _physics_process(delta: float) -> void:
+	
+	if Input.is_action_pressed("sprint"):
+		current_speed = sprint_speed
+	else:
+		current_speed = walk_speed
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "forward", "back")
+	direction = lerp(direction,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(),delta*lerp_speed)
+	if direction:
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
+
+	move_and_slide()
